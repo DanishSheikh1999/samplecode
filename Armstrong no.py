74326870dd2cef216f3f6024de214d1edcdc79b5
Swayam Kaul <43716674+swayamkaul@users.noreply.github.com>
2019-10-09 16:12:38 +0530

    Update Armstrong no.py

--- Armstrong no.py
+++ Armstrong no.py
@@ -1,10 +1,12 @@
+
+
 num = int(input("enter a number="))
 s = 0
 a = num
 while(num>0):
     dg = num%10
     s = s+dg**3
-    num = num//10
+    num = num/10
 if a==s:
     print("It is an armstrong number")
 else:
